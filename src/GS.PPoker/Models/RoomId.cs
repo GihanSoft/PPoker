2a2c68b013e7da879edf3b07b4e1d3d318609292
Mohammad Babayi <chief.mb1976@gmail.com>
2023-09-16 11:59:29 +0330

    refactor: Simplify id models with 'record struct's

--- src/GS.PPoker/Models/RoomId.cs
+++ src/GS.PPoker/Models/RoomId.cs
@@ -2,19 +2,11 @@ using System.Numerics;
 
 namespace GS.PPoker.Models;
 
-public readonly struct RoomId : IEquatable<RoomId>, IEqualityOperators<RoomId, RoomId, bool>, ISpanParsable<RoomId>
+public readonly record struct RoomId : IEquatable<RoomId>, IEqualityOperators<RoomId, RoomId, bool>, ISpanParsable<RoomId>
 {
     private readonly Guid _value;
-
     public RoomId(Guid guid) => _value = guid;
-
-    public readonly bool Equals(RoomId other) => _value.Equals(other._value);
-    public override int GetHashCode() => _value.GetHashCode();
-    public override bool Equals(object? obj) => obj is RoomId other && Equals(other);
     public override string ToString() => _value.ToString();
-
-    public static bool operator ==(RoomId left, RoomId right) => left._value == right._value;
-    public static bool operator !=(RoomId left, RoomId right) => left._value != right._value;
 
     public static RoomId Parse(ReadOnlySpan<char> s, IFormatProvider? provider) => Guid.Parse(s, provider);
     public static bool TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, out RoomId result)
--- src/GS.PPoker/Models/UserId.cs
+++ src/GS.PPoker/Models/UserId.cs
@@ -3,19 +3,11 @@ using System.Numerics;
 
 namespace GS.PPoker.Models;
 
-public readonly struct UserId : IEquatable<UserId>, IEqualityOperators<UserId, UserId, bool>
+public readonly record struct UserId : IEquatable<UserId>, IEqualityOperators<UserId, UserId, bool>
 {
     private readonly Guid _value;
-
     public UserId(Guid guid) => _value = guid;
-
-    public readonly bool Equals(UserId other) => _value.Equals(other._value);
-    public override int GetHashCode() => _value.GetHashCode();
-    public override bool Equals(object? obj) => obj is UserId other && Equals(other);
     public override string ToString() => _value.ToString();
-
-    public static bool operator ==(UserId left, UserId right) => left._value == right._value;
-    public static bool operator !=(UserId left, UserId right) => left._value != right._value;
 
     public static UserId Parse(ReadOnlySpan<char> s, IFormatProvider? provider) => Guid.Parse(s, provider);
     public static bool TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, out UserId result)
