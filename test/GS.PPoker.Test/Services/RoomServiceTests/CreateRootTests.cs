fcdf3e350e28975a8825944d24d25d3ff4a2b140
Mohammad Babayi <chief.mb1976@gmail.com>
2023-12-18 10:41:02 +0330

    test: add test for create room
    A test added for `RoomService.CreateRoom`

---
+++ test/GS.PPoker.Test/Services/RoomServiceTests/CreateRootTests.cs
@@ -0,0 +1,36 @@
+﻿using GS.PPoker.Models;
+
+namespace GS.PPoker.Test.Services.RoomServiceTests
+{
+    public class CreateRootTests : RoomServiceTestsBase
+    {
+        [Fact]
+        public void Should_CreateRoomWithOwnerAsOnlyMember_When_RoomJustCreated()
+        {
+            // arrange
+            UserId ownerId = Guid.NewGuid();
+            string ownerName = "owner";
+            string[] votes = _sut.DefaultVotes?.Split(',') ?? throw new InvalidOperationException("checked in ctor of base class");
+
+            // act
+            var roomId = _sut.CreateRoom(ownerId, ownerName, votes);
+            List<ReadOnlyRoom> observableCallStates = [];
+            _sut.AddObserver(roomId, imRoom => observableCallStates.Add(imRoom));
+
+            // assert
+            roomId.Should().NotBe(new RoomId(Guid.Empty));
+            observableCallStates.Count.Should().Be(1);
+            var room = observableCallStates[0];
+            room.Id.Should().Be(roomId);
+            room.OwnerId.Should().Be(ownerId);
+            room.AreVotesRevealed.Should().BeFalse();
+            room.AverageOfVotes.Should().Match(x => double.IsNaN(x));
+            room.PossibleVotes.AsEnumerable().Should().BeEquivalentTo(votes);
+            room.Members.Count.Should().Be(1);
+            var member = room.Members[0];
+            member.UserId.Should().Be(ownerId);
+            member.Name.Should().Be(ownerName);
+            member.Vote.Should().BeNull();
+        }
+    }
+}
---
+++ test/GS.PPoker.Test/Services/RoomServiceTests/RoomServiceTestsBase.cs
@@ -0,0 +1,58 @@
+﻿using GS.PPoker.Options;
+using GS.PPoker.Services;
+
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Time.Testing;
+
+namespace GS.PPoker.Test.Services.RoomServiceTests
+{
+    public abstract class RoomServiceTestsBase : IDisposable
+    {
+        protected const string DefaultVotes = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,20,22,24,26,28,30,32,34,36,38,40,44,48,52,56,60,64,68,72,80,88,96,104,112,∞";
+
+        private protected readonly IOptionsMonitor<RoomOptions> _options = Substitute.For<IOptionsMonitor<RoomOptions>>();
+        private protected readonly TimeProvider _timeProvider = new FakeTimeProvider();
+        private protected readonly RoomService _sut;
+        private bool disposedValue;
+
+        public RoomServiceTestsBase()
+        {
+            _options.CurrentValue.Returns(new RoomOptions
+            {
+                DefaultVotes = DefaultVotes,
+                IdleLifeSpan = TimeSpan.FromMinutes(10),
+            });
+            _sut = new(_options, _timeProvider);
+            _sut.DefaultVotes.Should().Be(DefaultVotes);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _sut.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~RoomServiceTestsBase()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
